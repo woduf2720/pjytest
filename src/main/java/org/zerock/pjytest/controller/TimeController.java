f9c3c130820058891ef9419a612778adb8cc7d41
woduf2720 <pjy5602@gmail.com>
2024-03-06 03:14:21 +0900

    project 생성

---
+++ src/main/java/org/zerock/pjytest/controller/TimeController.java
@@ -0,0 +1,41 @@
+package org.zerock.pjytest.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/time")
+@Log4j2
+@RequiredArgsConstructor
+public class TimeController {
+
+    private final DataSource dataSource;
+
+    @GetMapping("/now")
+    public Map<String, String> getNow() {
+        String now = "";
+
+        try (
+                Connection connection = dataSource.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement("select now()");
+                ResultSet resultSet = preparedStatement.executeQuery();
+        ){
+            resultSet.next();
+            now = resultSet.getString(1);
+            log.info("now: " + now);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return Map.of("NOW", now);
+    }
+}
--- src/test/java/org/zerock/pjytest/PjytestApplicationTests.java
+++ src/test/java/org/zerock/pjytest/PjytestApplicationTests.java
@@ -1,13 +1,34 @@
 package org.zerock.pjytest;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import javax.xml.transform.Result;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 @SpringBootTest
 class PjytestApplicationTests {
 
+	@Autowired
+	private DataSource dataSource;
+
 	@Test
 	void contextLoads() {
+		try (
+				Connection connection = dataSource.getConnection();
+				PreparedStatement preparedStatement = connection.prepareStatement("select now()");
+				ResultSet resultSet = preparedStatement.executeQuery();
+		){
+			resultSet.next();
+
+			System.out.println(resultSet.getString(1));
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
