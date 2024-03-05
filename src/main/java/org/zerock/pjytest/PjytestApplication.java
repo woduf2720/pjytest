aa54a39924f565c27f8183cb086a5aca4ae0b1fa
woduf2720 <pjy5602@gmail.com>
2024-03-06 01:25:09 +0900

    project 생성

---
+++ src/main/java/org/zerock/pjytest/PjytestApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.pjytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjytestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjytestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/zerock/pjytest/PjytestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.pjytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjytestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
